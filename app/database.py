359d785d47600c798b1b3217d257a0558e42daf8
baba <doum.contacts@gmail.com>
2024-10-02 14:24:16 +0000

    Update files and secrets

--- app/database.py
+++ app/database.py
@@ -1,5 +1,13 @@
-from databases import Database
 
-postgres_url = "postgresql://root:postges@139.144.63.238/mapaction"
-# postgres_url = "postgresql://postgres:postges@localhost/mapaction"
+from databases import Database
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the PostgreSQL URL from the environment variable
+postgres_url = os.getenv('POSTGRES_URL')
+
+# Initialize the database connection
 database = Database(postgres_url)
