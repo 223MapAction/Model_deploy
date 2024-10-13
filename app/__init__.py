5f2d5aa0499ad4ba1f8b233ace59275323220b4f
baba <doum.contacts@gmail.com>
2024-10-13 14:16:44 +0000

    Update celery_task

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
 # from .apis import prediction_endpoint
-from .services import predict, preprocess_image, perform_prediction, fetch_contextual_information, celery_app
+from .services import predict, preprocess_image, perform_prediction, fetch_contextual_information, celery_app, analyze_incident_zone
 from .models import ImageModel
