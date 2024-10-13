1df96e8f867171bbb6a35977bca8bb1cb6d48b3b
baba <doum.contacts@gmail.com>
2024-10-13 12:03:51 +0000

    Update servcies __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,4 +3,4 @@ from .llm import chat_response, get_response
 from .celery import fetch_contextual_information, perform_prediction, celery_app
 from .websockets import *
 from .analysis.satellite_data import download_sentinel_data, preprocess_sentinel_data
-from .analysis.incident_analysis import analyze_incident_zone, analyze_flood_extent, analyze_burn_area
+from .analysis.incident_analysis import analyze_incident_zone
