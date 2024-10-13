14a3abcbf05966b5c564a5f3eec5371c56ec2e9f
baba <doum.contacts@gmail.com>
2024-10-13 14:38:41 +0000

    Replace analysis function by task

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,6 @@
 from .cnn import predict, m_a_model, preprocess_image
 from .llm import chat_response, get_response
-from .celery import fetch_contextual_information, perform_prediction, celery_app
+from .celery import fetch_contextual_information, perform_prediction, celery_app, analyze_incident_zone
 from .websockets import *
 from .analysis.satellite_data import download_sentinel_data, preprocess_sentinel_data
-from .analysis.incident_analysis import analyze_incident_zone
+
