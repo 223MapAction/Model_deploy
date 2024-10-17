e13257509539fa0f5214a5ad7a777cb5ad24ec19
baba <doum.contacts@gmail.com>
2024-10-17 09:21:56 +0000

    Update model to use multi-labelling

--- app/services/cnn/cnn_model.py
+++ app/services/cnn/cnn_model.py
@@ -4,16 +4,15 @@ import torch
 from torchvision.models import resnet50, ResNet50_Weights
 import torch.nn as nn
 
-def m_a_model(num_classes: int):
+def m_a_model(num_tags: int):
     """
-    Initializes and modifies a ResNet50 model for the specified number of classes.
+    Initializes and modifies a ResNet50 model for multi-label classification.
     
     Args:
-        num_classes (int): The number of target classes for the model to classify.
+        num_tags (int): The number of tags to predict.
     
     Returns:
-        torch.nn.Module: A PyTorch model configured for the specified number of classes with
-                         the classifier layer tailored to the target task.
+        torch.nn.Module: A PyTorch model configured for multi-label classification.
     """
     # Load ResNet50 model with default weights
     resnet50_weights = ResNet50_Weights.DEFAULT
@@ -23,8 +22,14 @@ def m_a_model(num_classes: int):
     for param in model.parameters():
         param.requires_grad = False
     
-    # Modify the classifier (fully connected) layer to match num_classes
+    # Modify the classifier (fully connected) layer for multi-label classification
     num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, num_classes)
+    model.fc = nn.Sequential(
+        nn.Linear(num_ftrs, 512),
+        nn.ReLU(),
+        nn.Dropout(0.3),
+        nn.Linear(512, num_tags),
+        nn.Sigmoid()  # Use Sigmoid for multi-label output
+    )
     
     return model
