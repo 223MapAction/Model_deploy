edc26c0b63a77e1db9ba5b4f1647b2d488a1b657
immersir <129412569+immerSIR@users.noreply.github.com>
2025-03-25 16:27:50 +0000

    Fix tests

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,4 @@
-from .cnn import predict, preprocess_image
+from .cnn import predict
 from .llm import chat_response, get_response
 from .celery import fetch_contextual_information, perform_prediction, celery_app, analyze_incident_zone
 from .websockets import *
