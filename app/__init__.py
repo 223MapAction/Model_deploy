0f5d84aceed14d4c9b528ca3672074d03cd15a66
Mohamed Doumbia <129412569+immerSIR@users.noreply.github.com>
2026-05-13 11:18:10 -0400

    Revert "chore: save local changes before github push"

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,3 @@
-# Initialisation du module app (vide pour le sandbox afin d'éviter les conflits avec l'ancien code)
+# from .apis import prediction_endpoint
+from .services import predict, perform_prediction, fetch_contextual_information, celery_app, analyze_incident_zone
+from .models import ImageModel
